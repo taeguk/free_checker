7eab8ce375d2ff12e5995644fc9bcd8e323758f4
taeguk <xornrbboy@gmail.com>
2015-05-30 10:25:44 -0700

    i'm happy

---
+++ freeChecker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+c_file=$(dirname $0)"/fc.c"
+so_file=$(dirname $0)"/fc.so"
+#if [ ! -f "$so_file" ];then
+	echo "Making fc.so..."
+	if [ ! -f "$c_file" ];then
+		echo "$c_file 가 존재하지 않습니다.."
+		exit
+	fi
+	gcc -fPIC -shared -o $so_file $c_file -ldl
+	echo "Make Complete!"
+	echo
+#fi
+
+echo "----------free check start----------"
+
+if [ "$#" -eq "0" ]; then
+	echo "[Usage] $0 target_program target_program_parameters"
+else
+	cmd="LD_PRELOAD=$so_file $@"
+	eval $cmd
+fi
